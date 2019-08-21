feddf67b51f1f327e2d3e14833fb8caffcac09f3
Gustavo Kimura <gustavokimuraf@gmail.com>
2019-08-20 22:16:28 -0300

    Initial commit

---
+++ Assets/Scripts/MovimentacaoDoJogador.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MovimentacaoDoJogador: MonoBehaviour {
+    [SerializeField] private float forca;
+    private Transform transform;
+
+    void Start() {
+        this.transform = this.GetComponent<Transform>();
+    }
+
+    void Update() {
+        this.transform.Translate(Input.GetAxis("Horizontal") * this.forca, 0, 0);
+    }
+}
